f584b5ec5f3e7eee9d66b81c6e4d6ae19831a737
napoly <napolytan@protonmail.com>
2024-01-30 17:34:05 +0100

    Track point compression in LazyECPoint, rather than ECPoint.
    The reason is BouncyCastle 1.64 removed point compression tracking.
    
    Co-authored-by: Peter Dettman <peter.dettman@gmail.com>

--- core/src/main/java/org/bitcoinj/crypto/LazyECPoint.java
+++ core/src/main/java/org/bitcoinj/crypto/LazyECPoint.java
@@ -37,6 +37,7 @@ public class LazyECPoint {
 
     private final ECCurve curve;
     private final byte[] bits;
+    private final boolean compressed;
 
     // This field is effectively final - once set it won't change again. However it can be set after
     // construction.
@@ -53,6 +54,7 @@ public class LazyECPoint {
     public LazyECPoint(ECCurve curve, byte[] bits) {
         this.curve = curve;
         this.bits = bits;
+        this.compressed = ECKey.isPubKeyCompressed(bits);
     }
 
     /**
@@ -60,8 +62,9 @@ public class LazyECPoint {
      *
      * @param point      the wrapped point
      */
-    public LazyECPoint(ECPoint point) {
-        this.point = checkNotNull(point);
+    public LazyECPoint(ECPoint point, boolean compressed) {
+        this.point = checkNotNull(point).normalize();
+        this.compressed = compressed;
         this.curve = null;
         this.bits = null;
     }
@@ -72,17 +75,17 @@ public class LazyECPoint {
         return point;
     }
 
-    // Delegated methods.
-
-    public ECPoint getDetachedPoint() {
-        return get().getDetachedPoint();
-    }
-
     public byte[] getEncoded() {
         if (bits != null)
             return Arrays.copyOf(bits, bits.length);
         else
-            return get().getEncoded();
+            return get().getEncoded(compressed);
+    }
+
+    // Delegated methods.
+
+    public ECPoint getDetachedPoint() {
+        return get().getDetachedPoint();
     }
 
     public boolean isInfinity() {
@@ -106,10 +109,7 @@ public class LazyECPoint {
     }
 
     public boolean isCompressed() {
-        if (bits != null)
-            return ECKey.isPubKeyCompressed(bits);
-        else
-            return get().isCompressed();
+        return compressed;
     }
 
     public ECPoint multiply(BigInteger k) {
